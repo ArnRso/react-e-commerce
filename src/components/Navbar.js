b70007053d12282d8829630891f238d52f339b7d
ArnRso <adrien.russo@gmail.com>
2019-05-31 21:17:48 +0200

    57:00

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from Navbar</h3>
-      </div>
+      <nav className="navbar navbar-expand-small navbar-dark px-sm-5 bg-primary">
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
